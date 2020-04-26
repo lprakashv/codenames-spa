95361df8f5569cb0bd90c4e4b1aab6085f13ec9f
lpv0006 <lalit.vatsal@walmartlabs.com>
2020-04-26 23:56:57 +0530

    hints fix

--- src/WordBoard.js
+++ src/WordBoard.js
@@ -99,10 +99,11 @@ class WordBoard extends Component {
                         <p style={{textAlign: 'left'}}>1. Have 2 teams, one "red" (first turn) and the other blue.</p>
                         <p style={{textAlign: 'left'}}>2. Chose a person randomly from each team as a spy-master, only he/she can use the spy-master button and see the color of all the words</p>
                         <p style={{textAlign: 'left'}}>3. The spy master gives a "single" word as a hint and and a number (say 3).</p>
-                        <p style={{textAlign: 'left'}}>4. Then his/her team members choose/open different words their turn gets over as soon as they uncover the word of different color that their own team or they voluntarily end their turn.</p>
+                        <p style={{textAlign: 'left'}}>4. Then his/her team members choose/open different words (max 3, the number given by their spy master). Their turn gets over as soon as the team uncover any word of different color that their own team or they voluntarily end their turn.</p>
                         <p style={{textAlign: 'left'}}>5. Goal of each team is to finish up their color words.</p>
                         <p style={{textAlign: 'left'}}>6. There are 9 reds (first-turn), 8 blues, 7 greys (neutral) and one back words.</p>
                         <p style={{textAlign: 'left'}}>7. Team instantly loses if they uncover a black word.</p>
+                        <p style={{textAlign: 'left'}}>NOTE: RELOAD FOR A NEW GAME!</p>
                     </Jumbotron>
                 </Card.Header>
                 <Card.Body style={{ backgroundColor: (spyMaster) ? 'black' : 'lavender' }}>
