4bcd7c518ef2c532d2c06bf784b1141e46e5f7bb
lpv0006 <lalit.vatsal@walmartlabs.com>
2020-04-26 23:41:10 +0530

    fixed end turn and added instructions

--- src/WordBoard.js
+++ src/WordBoard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Button, Badge, Card, Jumbotron, ButtonGroup, Text } from 'react-bootstrap';
+import { Container, Row, Col, Button, Badge, Card, Jumbotron, ButtonGroup, ListGroup } from 'react-bootstrap';
 import { connect } from 'react-redux';
+import { SocialIcon } from 'react-social-icons';
 
 import { FINISH_TURN, OPEN, NEW_GAME, SPY_MASTER } from './BoardReducer';
 
@@ -42,7 +43,7 @@ function Tile(props) {
 
 class WordBoard extends Component {
     render() {
-        const { board, chooseWord, left, turn, gameOver, spyMaster, toggleSpymaster } = this.props;
+        const { board, chooseWord, left, turn, gameOver, spyMaster, toggleSpymaster, finishTurn } = this.props;
 
         function rowColumns(rowNum) {
             let cols = [];
@@ -81,17 +82,29 @@ class WordBoard extends Component {
 
         let lastRow =
             <ButtonGroup fluid vertical style={{ margin: 5 }}>
-                <Button variant={(turn == 'blue') ? 'primary' : 'danger'}>End {turn}'s Turn</Button>
+                <Button variant={(turn == 'blue') ? 'primary' : 'danger'} onClick={() => finishTurn()}>End {turn}'s Turn</Button>
                 <Button variant={(spyMaster) ? 'dark' : 'light'} onClick={() => toggleSpymaster()}>Spy Master</Button>
             </ButtonGroup>
 
         if (gameOver) {
-            lastRow = <h1>GAME OVER, WINNER IS "{(left.red == left.blue) ? 'BLUE' : 'RED'}"</h1>
+            lastRow = <h1>GAME OVER! THE WINNER IS "{(left.red == left.blue) ? <b style={{color: 'blue'}}>'BLUE'</b> : <b style={{color: 'red'}}>'RED'</b>}"</h1>
         }
 
         return (
             <Card style={{ borderRadius: 10 }}>
-                <Card.Header><Jumbotron fluid><h1>Codenames Game</h1></Jumbotron></Card.Header>
+                <Card.Header>
+                    <Jumbotron fluid style={{padding: 10}}>
+                        <h1>Codenames Game</h1>
+                        <h4 style={{textAlign: 'left'}}>Rules:</h4>
+                        <p style={{textAlign: 'left'}}>1. Have 2 teams, one "red" (first turn) and the other blue.</p>
+                        <p style={{textAlign: 'left'}}>2. Chose a person randomly from each team as a spy-master, only he/she can use the spy-master button and see the color of all the words</p>
+                        <p style={{textAlign: 'left'}}>3. The spy master gives a "single" word as a hint and and a number (say 3).</p>
+                        <p style={{textAlign: 'left'}}>4. Then his/her team members choose/open different words their turn gets over as soon as they uncover the word of different color that their own team or they voluntarily end their turn.</p>
+                        <p style={{textAlign: 'left'}}>5. Goal of each team is to finish up their color words.</p>
+                        <p style={{textAlign: 'left'}}>6. There are 9 reds (first-turn), 8 blues, 7 greys (neutral) and one back words.</p>
+                        <p style={{textAlign: 'left'}}>7. Team instantly loses if they uncover a black word.</p>
+                    </Jumbotron>
+                </Card.Header>
                 <Card.Body style={{ backgroundColor: (spyMaster) ? 'black' : 'lavender' }}>
                     <Container style={{ padding: 10 }}>
                         <Row fluid style={{margin: 5}}
@@ -102,14 +115,18 @@ class WordBoard extends Component {
                         {rows}
                         <Row
                             className="justify-content-md-center">
-                            <Col sm={3}>
+                            <Col sm={6}>
                                 {lastRow}
                             </Col>
                         </Row>
                     </Container>
                 </Card.Body>
                 <Card.Footer>
-                    Made by Lalit Prakash Vatsal
+                <p>Made by Lalit Prakash Vatsal</p>
+                <SocialIcon url="https://twitter.com/lprakashv" label='twitter' />
+                <SocialIcon url="https://medium.com/lprakashv" label='medium' />
+                <SocialIcon url="https://www.linkedin.com/in/lalit-vatsal-ab921879/" label='linkedin' />
+                <SocialIcon url="https://github.com/lprakashv" label='github' />
                 </Card.Footer>
             </Card>
         );
