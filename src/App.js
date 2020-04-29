e2246dfd4b6a59d0e980c367067c4482380a6c64
lpv0006 <lalit.vatsal@walmartlabs.com>
2020-04-29 16:04:53 +0530

    1. new-game. 2. code organized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import WordBoard from './WordBoard';
+import WordBoard from './components/WordBoard';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import reducer from './BoardReducer';
+import reducer from './reducers/BoardReducer';
+
+export const store = createStore(reducer);
 
 function App() {
-  const store = createStore(reducer);
-
   return (
     <div className="App">
       <Provider store={store}>
