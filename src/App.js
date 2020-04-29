ee844f264129c400fd933512b7974c10ac25918a
lpv0006 <lalit.vatsal@walmartlabs.com>
2020-04-29 23:15:53 +0530

    Added redux-dev-tools for dev env

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import WordBoard from './components/WordBoard';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 
 import reducer from './reducers/BoardReducer';
 
-export const store = createStore(reducer);
+export const store = (process.env.NODE_ENV === 'production') ? createStore(reducer) : createStore(reducer, devToolsEnhancer({}));
 
 function App() {
   return (
